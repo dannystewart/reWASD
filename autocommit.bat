e2231dc2b0cda5e59d0e7aa4b92f4d820c398aa4
Danny Stewart <danny@stewart.cc>
2022-02-20 21:34:10 -0500

    auto commit on change

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,3 @@
 c:
 cd c:\Users\Public\Documents\reWASD\
-"%PROGRAMFILES%\git\bin\sh.exe" --login -i -c "git add . && git commit -am 'auto commit on change'"
+"%PROGRAMFILES%\git\bin\sh.exe" --login -i -c "git add . && git commit -am 'auto commit on change' && git push"
